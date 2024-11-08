3775f0131c487f88f7fa9faeb169579bce4baed9
binodtamang24 <67016236+B-nod@users.noreply.github.com>
2024-11-08 11:02:40 +0545

    cover

--- src/pages/userpages/Hompage.jsx
+++ src/pages/userpages/Hompage.jsx
@@ -5,13 +5,13 @@ import cover from '../../assests/cover.jpg'
 const Hompage = () => {
   return (
     <>
-    <div className="relative bg-gradient-to-r from-purple-900 to-indigo-800 py-16 font-[sans-serif]">
+    <div className="relative bg-gradient-to-r from-cyan-900 to-green-400 py-16 font-[sans-serif]">
       <div className="absolute inset-0">
         <img src={cover} alt="Background Image" className="w-full h-full object-cover opacity-50" />
       </div>
 
       <div className="relative max-w-screen-lg mx-auto px-8 z-10 text-center text-white">
-        <h1 className="text-4xl md:text-5xl font-extrabold leading-tight mb-6">Welcome to Our Premium Service</h1>
+        <h1 className="text-4xl md:text-5xl font-extrabold leading-tight mb-6">Welcome to Our Eagle Trend Store</h1>
         <p className="text-lg md:text-xl mb-12">Experience excellence like never before with our exclusive products and services.</p>
         <button type="button" className="bg-indigo-600 hover:bg-indigo-700 text-white text-base tracking-wide px-6 py-3 rounded-full transition duration-300 ease-in-out shadow-lg hover:shadow-xl">Get Started</button>
       </div>
